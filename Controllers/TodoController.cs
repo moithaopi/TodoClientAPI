9b13cd4b5927ebc826336c438b69bd563481ceb7
Moithaopi Monwela <mmonwela@botswanapost.co.bw>
2020-11-11 11:26:38 +0200

    Initial Commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TodoClientApi.Models;
+
+namespace TodoClientApi.Controllers
+{
+    public class TodoController : Controller
+    {
+        private HttpClient _HttpClient;
+        private Todo _todo;
+        public TodoController(HttpClient httpClient,Todo todo)
+        {
+            _todo = todo;
+            _HttpClient = httpClient;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public async Task<Todo> getTodos()
+        {
+            var response = await _HttpClient.GetAsync("https://localhost:5001/api/todo");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                _todo = JsonConvert.DeserializeObject<Todo>(content);
+            }
+            return _todo;
+        }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoClientApi.Models
+{
+    public class Todo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public bool isComplete { get; set; }
+    }
+}
